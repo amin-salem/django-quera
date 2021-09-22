5fcdd7925a69bcdef56db871d960d9c21558b4c6
amin-salem <aminsaalem@gmail.com>
2021-09-22 13:16:43 +0330

    view and book_detail template

--- library/views.py
+++ library/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.http.response import HttpResponse
+from django.shortcuts import render, HttpResponse
+from .models import Book
 # Create your views here.
+
+def book_detail(request, book_id):
+    try:
+        book = Book.objects.get(pk = book_id)
+        context = {'book': book}
+        return render(request, 'book_detail.html', context = context)
+    except Book.DoesNotExist:
+        return HttpResponse('<h2> Book matching query does not exist. </h2>')
